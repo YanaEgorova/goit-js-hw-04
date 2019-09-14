285c0e35cb031b1e5b9222bfd629bf008b3d59ae
YanaEgorova <yanaegorova69.3@gmail.com>
2019-09-14 03:10:31 +0300

    +

--- task-2.js
+++ task-2.js
@@ -1,1 +1,36 @@
 'use strict';
+
+//Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+
+const inventory = {
+  items: ['Монорельса', 'Фильтр'],
+  add(itemName) {
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryOperation = function (itemName, inventoryAction) {
+  console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+  inventoryAction(itemName);
+};
+
+
+
+
+
+
+
+invokeInventoryOperation('Аптечка', inventory.add);
+
+// Invoking add opeartion on Аптечка
+
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+invokeInventoryOperation('Фильтр', inventory.remove);
+// Invoking remove opeartion on Фильтр
+
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
